80f23ccc0e35167daf37eda1aad9f3acfebf1b07
Jost2167 <johnstiven2000@gmail.com>
2025-05-22 13:41:02 -0500

    validar a nivel de modelo

--- Models/TipoCuenta.cs
+++ Models/TipoCuenta.cs
@@ -3,13 +3,31 @@ using ManejoPresupuesto.Validations;
 
 namespace ManejoPresupuesto.Models;
 
-public class TipoCuenta
+public class TipoCuenta //: IValidatableObject
 {
     public int Id { get; set; }
     
     [Required(ErrorMessage = "El {0} es requerido.")]
-    [PrimeraLetraMayuscula]
     public string Nombre { get; set; }
     public int Orden { get; set; }
     public int UsuarioId { get; set; }
+    
+    // Validacion desde el modelo
+    /*
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(Nombre))
+        {
+            string primeraLetra = Nombre[0].ToString();
+
+            if (primeraLetra != primeraLetra.ToUpper())
+            {
+                // yield permite devolver elementos uno a uno desde metodos que implementen IEnumerable
+                yield return new ValidationResult("La primera letra debe ser mayúscula.", 
+                    new []{ nameof(Nombre)});
+            }
+        }
+        yield break;
+    }
+    */
 }
